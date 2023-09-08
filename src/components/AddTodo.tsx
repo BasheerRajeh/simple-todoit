6ef3daff2617eed8806b977ce80aa0a498a0f471
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-09-08 22:37:08 +0300

    make todo required

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -26,6 +26,7 @@ function AddTodo({ onAdd }: AddTodoProps) {
                 className="text-3xl flex-grow px-2 pt-4 pb-1 border-b-2 focus-within:border-b-4 focus:border-b-4 border-gray-200 focus:border-gray-400 focus-within:border-gray-400 outline-none transition-all"
                 value={title}
                 placeholder="Add Todo"
+                required
                 onChange={(evt) => setTitle(evt.target.value)}
             />
             <Button aria-label="Add Todo">
